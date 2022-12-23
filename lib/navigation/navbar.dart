6308bf20f8ecc49574fbd89e328f27494b64a2f1
Anish Khanal <anishkhanal27@gmail.com>
2022-12-23 12:05:29 +0545

    Navbar

---
+++ lib/navigation/navbar.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'aboutScreen.dart';
+import 'categoryScreen.dart';
+import 'homeScreen.dart';
+import 'profileScreen.dart';
+
+class BottomNavigationScreen extends StatefulWidget {
+  const BottomNavigationScreen({super.key});
+
+  @override
+  State<BottomNavigationScreen> createState() => _BottomNavigationScreenState();
+}
+
+class _BottomNavigationScreenState extends State<BottomNavigationScreen> {
+  int _selectedIndex = 0;
+
+  List<Widget> listBottomScreens = [
+    const HomeScreen(),
+    const AboutScreen(),
+    const CategoryScreen(),
+    const ProfileScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(''),
+        centerTitle: true,
+      ),
+      body: listBottomScreens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.amber,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.black,
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _selectedIndex,
+        onTap: (index) {
+          setState(
+            () {
+              _selectedIndex = index;
+            },
+          );
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.dashboard),
+            label: 'Categories',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_view_day_rounded),
+            label: 'Appointment',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
